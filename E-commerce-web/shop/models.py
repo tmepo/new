b3400d5667c6633d5568b14a4bfab4c6a5f7e973
tempog0 <tempforall2024@gmail.com>
2024-05-28 06:05:00 +0545

    added images

--- E-commerce-web/shop/models.py
+++ E-commerce-web/shop/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 class product(models.Model):
     product_id=models.AutoField
-    # category=models.CharField(max_length=30)
-    # price=models.IntegerField(default=0)
+    category=models.CharField(max_length=30)
+    price=models.IntegerField(default=0)
     product_name=models.CharField(max_length=40)
     discription=models.CharField(max_length=200,default='')
     publishdate=models.DateField()
-    # image=models.ImageField(upload_to='shop/images',default='')
+    image=models.ImageField(upload_to='shop/images',default='')
 
     def __str__(self) -> str:
         return self.product_name
